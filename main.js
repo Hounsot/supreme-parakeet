2b2bc90a781a3740b40abf69b7dd3aa00c3ee560
Matvey Vasilyev <hounsout@yandex.ru>
2024-11-27 12:04:24 +0300

    init commit

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 import './style.css'
 
 document.addEventListener("DOMContentLoaded", () => {
+  const body = document.body;
+  document.querySelector('#OpenButton').addEventListener('click', function() {
+    const floatForm = document.querySelector('.A_FloatForm');
+    const floatFormBG = document.querySelector('.O_Form');
+    if (floatForm) {
+      floatFormBG.classList.add('U_Open');
+      floatForm.classList.add('U_Open');
+      body.classList.add('no-scroll'); // Disable body scroll
+    }
+  });  
+  document.querySelector('.A_BurgerCloseButton').addEventListener('click', function() {
+    const floatForm = document.querySelector('.A_FloatForm');
+    const floatFormBG = document.querySelector('.O_Form');
+    if (floatForm) {
+      floatFormBG.classList.remove('U_Open');
+      floatForm.classList.remove('U_Open');
+      body.classList.remove('no-scroll'); // Enable body scroll
+    }
+  });  
+  document.querySelector('.W_MarkContainer').addEventListener('click', function() {
+    const checkmark = document.querySelector('.A_Mark');
+    if (checkmark) {
+      checkmark.classList.toggle('Active');
+    }
+  });  
   // Select all project cards
   const projectCards = document.querySelectorAll(".M_Project");
 
